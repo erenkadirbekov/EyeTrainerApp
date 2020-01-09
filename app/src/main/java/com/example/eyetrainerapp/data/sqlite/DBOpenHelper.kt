94f94d18ef7ed368e08f2bdff98075963a6cdd68
Yeren Kadirbekov <erenkadirbekov@gmail.com>
2020-01-09 13:53:11 +0600

    Installing local database

--- app/src/main/java/com/example/eyetrainerapp/data/sqlite/DBOpenHelper.kt
+++ app/src/main/java/com/example/eyetrainerapp/data/sqlite/DBOpenHelper.kt
@@ -1,56 +1,47 @@
 package com.example.eyetrainerapp.data.sqlite
 
-import android.content.ContentValues
+
 import android.content.Context
+import android.content.SharedPreferences
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteException
 import android.database.sqlite.SQLiteOpenHelper
 import com.example.eyetrainerapp.data.entities.ExerciseEntity
+import java.io.File
+import java.io.FileOutputStream
+import java.lang.RuntimeException
 
-class DBOpenHelper(context: Context) : SQLiteOpenHelper(context,DATABASE_NAME,null,DATABASE_VERSION) {
+class DBOpenHelper(val context: Context) : SQLiteOpenHelper(context,DATABASE_NAME,null,DATABASE_VERSION) {
     companion object {
         private val DATABASE_VERSION = 3
-        private val DATABASE_NAME = "EyeTrainerAppDB"
+        private val DATABASE_NAME = "eyeTrainerAppDB"
         private val TABLE_EXERCISES = "exercises"
-        private val KEY_ID = "id"
-        private val KEY_NAME = "name"
-        private val KEY_DESCRIPTION = "description"
-        private val KEY_INSTRUCTION = "instruction"
+        private const val ASSETS_PATH = "databases"
     }
-    override fun onCreate(db: SQLiteDatabase?) {
-        // TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        val CREATE_EXERCISES_TABLE = ("CREATE TABLE IF NOT EXISTS " + TABLE_EXERCISES + "("
-                + KEY_ID + " INTEGER PRIMARY KEY," + KEY_NAME + " TEXT,"
-                + KEY_DESCRIPTION + " TEXT,"
-                + KEY_INSTRUCTION + " TEXT"+ ")")
-        db?.execSQL(CREATE_EXERCISES_TABLE)
 
+    private val sharedPreferences: SharedPreferences = context.getSharedPreferences(
+        "${context.packageName}.database_versions",
+        Context.MODE_PRIVATE
+    )
+
+    private fun installedDatabaseIsOutdated() : Boolean {
+        return sharedPreferences.getInt(DATABASE_NAME, 0) < DATABASE_VERSION
+    }
+
+    private fun writeDatabaseVersionInPreferences() {
+        sharedPreferences.edit().apply {
+            putInt(DATABASE_NAME, DATABASE_VERSION)
+            apply()
+        }
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
 
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        //  TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-        db!!.execSQL("DROP TABLE IF EXISTS " + TABLE_EXERCISES)
-        onCreate(db)
-    }
 
-    fun addExercise(exercise: ExerciseEntity):Long{
-        val database = this.writableDatabase
-        var contentValues = ContentValues()
-            contentValues.put(KEY_ID, exercise.id)
-            contentValues.put(KEY_NAME, exercise.name)
-            contentValues.put(KEY_DESCRIPTION, exercise.description)
-            contentValues.put(KEY_INSTRUCTION, exercise.instruction)
-            // Inserting Row
-            val success = database.insert(TABLE_EXERCISES, null, contentValues)
-
-            contentValues.clear()
-            //2nd argument is String containing nullColumnHack
-
-        System.out.println("I'm " + success)
-        database.close() // Closing database connection
-        return success
     }
 
     fun getAllExercises():List<ExerciseEntity>{
@@ -116,5 +107,33 @@ class DBOpenHelper(context: Context) : SQLiteOpenHelper(context,DATABASE_NAME,nu
             } while (cursor.moveToNext())
         }
         return exercise
+    }
+
+    fun installDatabaseFromAssets() {
+        val inputStream = context.assets.open("$ASSETS_PATH/$DATABASE_NAME.db")
+        try {
+            val outputFile = File(context.getDatabasePath(DATABASE_NAME).path)
+            val outputStream = FileOutputStream(outputFile)
+
+            inputStream.copyTo(outputStream)
+            inputStream.close()
+        }
+        catch (exception: Throwable) {
+            throw RuntimeException("$DATABASE_NAME cannot be installed", exception)
+        }
+    }
+
+    @Synchronized
+    private fun installOrUpdateIfNecessary() {
+        if (installedDatabaseIsOutdated()) {
+            context.deleteDatabase(DATABASE_NAME)
+            installDatabaseFromAssets()
+            writeDatabaseVersionInPreferences()
+        }
+    }
+
+    override fun getReadableDatabase(): SQLiteDatabase {
+        installOrUpdateIfNecessary()
+        return super.getReadableDatabase()
     }
 }
--- app/src/main/java/com/example/eyetrainerapp/ui/activities/home/HomeActivity.kt
+++ app/src/main/java/com/example/eyetrainerapp/ui/activities/home/HomeActivity.kt
@@ -1,39 +1,17 @@
 package com.example.eyetrainerapp.ui.activities.home
 
-import android.annotation.SuppressLint
-import android.content.Intent
-import android.database.Cursor
-import android.database.sqlite.SQLiteDatabase
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.LinearLayout
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProviders
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.eyetrainerapp.R
 import com.example.eyetrainerapp.data.entities.ExerciseEntity
 import com.example.eyetrainerapp.data.sqlite.DBOpenHelper
-import com.example.eyetrainerapp.domain.HomeViewModel
-import com.example.eyetrainerapp.domain.HomeViewModelFactory
 import com.example.eyetrainerapp.utils.startExerciseActivity
-import com.example.eyetrainerapp.utils.startHomeActivity
-import org.kodein.di.KodeinAware
-import org.kodein.di.android.kodein
-import org.kodein.di.generic.instance
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-
-
-
-
-
 
 class HomeActivity : AppCompatActivity() {
 
-
-
+    private val DATABASE_NAME = "EyeTrainerAppDB"
     private lateinit var dbHelper: DBOpenHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,32 +21,10 @@ class HomeActivity : AppCompatActivity() {
         System.out.println("I'm here 4")
 
         dbHelper = DBOpenHelper(this)
-        var database: SQLiteDatabase = baseContext.openOrCreateDatabase("EyeTrainerAppDB.db", MODE_PRIVATE, null)
-        dbHelper.onCreate(database)
 
-        if (isTableEmpty(database, "exercises")) {
-            System.out.println("I'm here 3")
-            val exercise1 = ExerciseEntity(
-                1,
-                "Focus change",
-                "This exercise works by challenging your focus. It should be done from a seated position.",
-                "Hold your pointer finger a few inches away from your eye. Focus on your finger. Slowly move your finger away from your face, holding your focus. Look away for a moment, into the distance. Focus on your outstretched finger and slowly bring it back toward your eye. Look away and focus on something in the distance. Repeat three times."
-            )
-            val exercise2 = ExerciseEntity(
-                2,
-                "Near and far focus",
-                "This is another focus exercise. As with the previous one, it should be done from a seated position.",
-                "Hold your thumb about 10 inches from your face and focus on it for 15 seconds. Find an object roughly 10 to 20 feet away, and focus on it for 15 seconds. Return your focus to your thumb. Repeat five times."
-            )
-            val exercises: ArrayList<ExerciseEntity> = ArrayList()
-            exercises.add(exercise1)
-            exercises.add(exercise2)
-            dbHelper.addExercise(exercise1)
-            dbHelper.addExercise(exercise2)
 
-        }
 
-        var recyclerView = findViewById<RecyclerView>(com.example.eyetrainerapp.R.id.myRecycler)
+        var recyclerView = findViewById<RecyclerView>(R.id.myRecycler)
         val items: List<ExerciseEntity> = dbHelper.getAllExercises()
 
 
@@ -80,24 +36,5 @@ class HomeActivity : AppCompatActivity() {
         })
 
         recyclerView.adapter = adapter
-    }
-
-    fun isTableEmpty(database: SQLiteDatabase, tableName: String): Boolean {
-
-        val flag: Boolean
-        val quString = "select exists(select 1 from $tableName);"
-
-        val cursor = database.rawQuery(quString, null)
-        cursor.moveToFirst()
-        val count = cursor.getInt(0)
-        if (count == 1) {
-            flag = false
-        } else {
-            flag = true
-        }
-        cursor.close()
-        database.close()
-
-        return flag
     }
 }
